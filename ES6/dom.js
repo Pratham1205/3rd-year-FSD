43718518a644a5d54e10a80ae319133ca437417d
Pratham1205 <007pratham2005@gmail.com>
2025-10-13 12:02:37 +0530

    physical dom

---
+++ ES6/dom.js
@@ -0,0 +1,9 @@
+const div=document.createElement('div');
+const h1=document.createElement('h1');
+h1.innerHTML="Hi we are java developers";
+div.appendChild(h1);
+const id=document.getElementById('root');
+id.appendChild(div);
+
+
+
